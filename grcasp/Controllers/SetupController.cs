071fd8a09bc12ee0e23caee9f038487306ab78a7
Chiwawaweb <git@chiwawaweb.com>
2018-04-30 00:22:56 +0200

    Maj

---
+++ grcasp/Controllers/SetupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace grcasp.Controllers
+{
+    public class SetupController : Controller
+    {
+        // GET: Setupt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ grcasp/Controllers/VentesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace grcasp.Controllers
+{
+    public class VentesController : Controller
+    {
+        // GET: Ventes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
