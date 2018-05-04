cb02bc025fc679efc91edb6ac4897cde12fc2cfd
Chiwawaweb <git@chiwawaweb.com>
2018-05-04 19:48:35 +0200

    Maj

--- grcasp/Models/Client.cs
+++ grcasp/Models/Client.cs
@@ -15,6 +15,15 @@ namespace grcasp.Models
         public string Nom { get; set; }
         public string Adresse1 { get; set; }
         public string Adresse2 { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+        public string Pays { get; set; }
+        public string TypeTel1 { get; set; }
+        public string Tel1 { get; set; }
+        public string TypeTel2 { get; set; }
+        public string Tel2 { get; set; }
+        public string TypeTel3 { get; set; }
+        public string Tel3 { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
